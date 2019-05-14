8da2974aef983f82c116109bf1c34ba790d9c588
FerOrduna <A01422919@itesm.mx>
2019-05-13 20:06:18 -0500

    admin con get dashborad y get control panel

--- api/routes/admin.js
+++ api/routes/admin.js
@@ -1,2 +1,24 @@
-console.log("Hola Fernuchis");
-console.log("Hice un cambio jjeje");
+const express = require('express');
+const router = express.Router();
+const { isAuthenticated, isAdminAuthenticated } = require('../helpers/auth');
+const passport = require('passport');
+const connection = require('../../database');
+
+router.get('/controlpanel', isAdminAuthenticated, (req, res) => {
+    res.render('admin/prueba');
+});
+
+router.get('/dashboardU', (req, res) => {
+
+    let query = 'SELECT * from usuarios';
+
+    connection.query(query, (error, rows, fields) => {
+
+        if (!error) {
+            res.render('admin/usuarios', { usuarios: rows });
+        }
+
+    });
+
+
+});
